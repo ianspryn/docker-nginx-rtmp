b92d171845e2cf95e4685f1c85243d5442539e04
Ian Spryn <4182500+ianspryn@users.noreply.github.com>
2020-01-09 13:35:47 -0500

    Handle new edge case
    If streaming started at 11 AM, but the preaching recording was generated after 12 PM, then there is an AM/PM difference, and the files will not match.
    
    Now, the script will not check for AM/PM to match and instead look at the most recently created file in the preaching directory

--- process-files.sh
+++ process-files.sh
@@ -2,9 +2,26 @@ BASENAME=$1
 DIRNAME=/nginx/flv
 FILEPATH=$DIRNAME/$BASENAME.flv
 
+# https://stackoverflow.com/questions/965053 - CLIPPING
+# Clip $BASENAME. EXAMPLE: '200107PM.213853.mp4' becomes '200107PM'
+DATE=${BASENAME%.*}
+# Remove the period (AM/PM) because what if the morning service preaching starts
+# after 12 PM, but the full service started at 11 AM? We can't rely on using the
+# AM/PM as part of our file searching to match up the sermons
+# Example: '200109PM' becomes '200109'
+DATE_NO_PERIOD="$(echo $DATE | sed -e 's/[A-Z]*$//')"
+
 PREACHING_DIRECTORY=/nginx/processed/Preaching
 FULL_SERVICE_DIRECTORY=/nginx/processed/Full\ Service
 FULL_SERVICE_MP3_DIRECTORY=/nginx/processed/Full\ Service\ MP3
+
+# https://unix.stackexchange.com/questions/283886 - REGEX
+# Find the first most recently created file in the preaching folder that contains the basename date
+# and remove the filename extension and date.
+# We do this now incase encoding below takes forever and someone starts another stream
+# EXAMPLE: '200107AM - Bro. Paul LaFontaine - Great sermon.mp4' becomes ' - Bro. Paul LaFontaine - Great sermon'
+cd $PREACHING_DIRECTORY
+PREACHING_FILENAME=$(ls -t $DATE_NO_PERIOD*.mp4 | head -1 | sed -e 's/^[0-9]*[A-Z]\{0,2\}//' -e 's/\.mp4$//')
 
 ##### Clean Up #####
 # Remove all files that are 1 bytes or less (0 bytes wasn't reliable)
@@ -28,15 +45,7 @@ then
 fi
 
 ##### Rename Full Service MP4 and MP3 based on filename in Preaching folder #####
-cd $PREACHING_DIRECTORY
-# https://stackoverflow.com/questions/965053 - CLIPPING
-# Clip $BASENAME. EXAMPLE: '200107PM.213853.mp4' becomes '200107PM'
-DATE=${BASENAME%.*}
-# https://unix.stackexchange.com/questions/283886 - REGEX
-# Find the first file in the preaching folder that contains the basename date
-# and remove the filename extension and date.
-# EXAMPLE: '200107AM - Bro. Paul LaFontaine - Great sermon.mp4' becomes ' - Bro. Paul LaFontaine - Great sermon'
-FILENAME="$DATE$(ls $DATE*.mp4 | head -1 | sed -e 's/^[0-9]*[A-Z]\{0,2\}//' -e 's/\.mp4$//')"
+FILENAME=$DATE$PREACHING_FILENAME
 
 # Finally, we rename the files
 mv -n "$FULL_SERVICE_DIRECTORY/$BASENAME.mp4" "$FULL_SERVICE_DIRECTORY/$FILENAME.mp4"
